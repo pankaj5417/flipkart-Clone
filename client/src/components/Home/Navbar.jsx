92e5bd5be799c4fe3556772aa1d4ca2353c970ca
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-05-12 20:04:43 +0530

    updated the json data

--- client/src/components/Home/Navbar.jsx
+++ client/src/components/Home/Navbar.jsx
@@ -1,43 +1,52 @@
-import { Box, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import { Box, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
-import React from 'react';
-import { navData } from '../../constants/data';
+import React, { useEffect, useState } from "react";
+//import { navData } from '../../constants/data';
 
-const useStyle=makeStyles({
-    component:{
-        display:"flex",
-        margin:"55px 130px 0 130px",
-        justifyContent:"space-between"
-
-    },
-    container:{
-        textAlign:"center",
-        padding:"12px 8px"
-    },
-    image:{
-        width:64
-    },
-    text:{
-        fontSize:14,
-        fontWeight:600
-
-
-    }
-})
+const useStyle = makeStyles({
+  component: {
+    display: "flex",
+    margin: "55px 130px 0 130px",
+    justifyContent: "space-between",
+  },
+  container: {
+    textAlign: "center",
+    padding: "12px 8px",
+  },
+  image: {
+    width: 64,
+  },
+  text: {
+    fontSize: 14,
+    fontWeight: 600,
+  },
+});
 
 export default function Navbar() {
+  const [navData, setNavData] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
 
-    const classes=useStyle()
+  const getData = () => {
+    fetch(`http://localhost:3001/navData`)
+      .then((d) => d.json())
+      .then((res) => {
+        console.log(res);
+        setNavData(res);
+      });
+  };
+
+  const classes = useStyle();
   return (
-      <Box className={classes.component}>
-          {navData.map(data=>(
-              <Box>
-                  <img src={data.url} alt="" className={classes.image} />
-               <Typography className={classes.text}>{data.text}</Typography>
-
-              </Box>
-          ))}
-      </Box>
-  )
+    <Box className={classes.component}>
+      {navData.map((data) => (
+        <Box>
+          <img src={data.url} alt="" className={classes.image} />
+          <Typography className={classes.text}>{data.text}</Typography>
+        </Box>
+      ))}
+    </Box>
+  );
 }
